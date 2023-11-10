7fb4c519acb4814e37d5644a0927b7104186aecc
Caleb Frankenberger <mail@calebfrankenberger.com>
2023-11-10 17:21:33 -0500

    Date time format

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { DateTime} = require("luxon");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/');
   eleventyConfig.addPassthroughCopy('src/css/');
@@ -9,6 +11,14 @@ module.exports = function (eleventyConfig) {
     return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md');
   });
 
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  }); 
+
+  eleventyConfig.addFilter("postDate2", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
+  }); 
+
   return {
     dir: {
       input: 'src',
@@ -19,5 +29,5 @@ module.exports = function (eleventyConfig) {
     markdownTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
-  };
+  };  
 };
