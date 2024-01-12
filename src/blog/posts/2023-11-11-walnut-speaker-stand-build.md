0d4fecb4242526aa9a69226f5e14fa1902aa8ee8
Caleb Frankenberger <mail@calebfrankenberger.com>
2024-01-11 21:06:57 -0500

    Updated Site

--- src/blog/posts/2023-11-11-walnut-speaker-stand-build.md
+++ src/blog/posts/2023-11-11-walnut-speaker-stand-build.md
@@ -2,16 +2,17 @@
 layout: layouts/post.html
 title: Walnut Speaker Stand Build
 date: 2023-01-13
-updated: ""
+updated: ''
 tags:
   - woodworking
   - office
 ---
+
 # Intro
 
 My Dad got me some new Sony speakers for Christmas, and with three monitors I had no idea where to place them. If I laid them horizontally they didn't point toward my ears or sound very good. If I placed them toward the edges of the desk and angled them in, the monitors got in the way of the sound.
 
-My solution for this was to build speaker stands that rise above the monitor and angle toward my ears. This is definitely the best solution I've found for speaker placement if you have three monitors. 
+My solution for this was to build speaker stands that rise above the monitor and angle toward my ears. This is definitely the best solution I've found for speaker placement if you have three monitors.
 
 # Prototype
 
@@ -19,50 +20,24 @@ I started by sitting at my computer, holding a speaker in the air behind my moni
 
 In order to test the angles I made a mockup out of some scrap wood I have. For this I just used my table saw to get the speaker platforms cut to size, and then my miter saw to cut the angle correctly.
 
-![Speaker platform](/assets/images/1.jpeg "Speaker platform")
-
 The size of the speaker platforms is just slightly larger than the bottom of the speakers. Plus an added lip to prevent them from sliding off.
-
-![Four speaker legs](/assets/images/2.jpeg "Four speaker legs")
 
 The legs were cut 90 degrees on one side and at an angle on the other, to hold the speakers at a slant.
 
-![Image of assembled prototype](/assets/images/3.jpeg "Assembled prototype")
-
 For placement of the speaker platform onto the legs, I just did a dry fit of everything and moved around the speaker platform with the speaker on it until I felt like it was stable, then I marked and measured that spot so I could make sure I get it exact when I glued them together.
-
-
 
 # Prototype Results
 
-![My desk with the speaker stands + speakers on it](/assets/images/4.jpeg "Prototypes were a success.")
-
-The prototypes turned out just like I'd hoped, and I used them for a week or two before making the real stands. 
-
-
+The prototypes turned out just like I'd hoped, and I used them for a week or two before making the real stands.
 
 # Round 2
 
-![The pieces laid out to be assembled](/assets/images/5.jpeg "Walnut pieces")
-
-I chose to make the real stands out of black walnut. The process for making these was the exact same as the prototypes, except I used a couple router bits on most of the pieces to give it character. 
-
-
-
-![Assembled speaker stand, unfinished](/assets/images/6.jpeg "Unfinished stand")
+I chose to make the real stands out of black walnut. The process for making these was the exact same as the prototypes, except I used a couple router bits on most of the pieces to give it character.
 
 The first assembly, things are looking good.
 
-![Speaker stands with oil on them, drying](/assets/images/7.jpeg "Finishing the stands")
-
-The stands got finished with many(can't remember how many) coats of danish oil. 
+The stands got finished with many(can't remember how many) coats of danish oil.
 
 # Final Product
-
-![A single speaker on a speaker stand](/assets/images/8.jpeg "Finished product 1")
-
-![Front view of desk with speaker stands](/assets/images/9.jpeg "Finished product 2")
-
-![An angled view of my whole desk](/assets/images/10.jpeg "Finished product 3")
 
 I think they came out great and I'm very happy to be able to switch between my headphones and speakers now. This was an easy build and I'd recommend it to anyone who has three monitors and wants to get speakers for their setup.
--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,79 +1,83 @@
-// This file was taken from 
+// This file was taken from
 // https://github.com/TehBrian/tehbrian.github.io/blob/main/assets/js/theme.js
+// and modified for my use
 
-const themeSwitch = document.getElementById("Switch");
-const mobileSun = document.getElementById("sun");
-const mobileMoon = document.getElementById("moon");
-
+const themeToggle = document.getElementById('themeToggleCheckbox');
 
 function onLoad() {
-    // add listeners for system color-change events.
-    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", e => e.matches && activateDarkTheme());
-    window.matchMedia("(prefers-color-scheme: light)").addEventListener("change", e => e.matches && activateLightTheme());
+  // add listeners for system color-change events.
+  window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', (e) => e.matches && activateDarkTheme());
+  window
+    .matchMedia('(prefers-color-scheme: light)')
+    .addEventListener('change', (e) => e.matches && activateLightTheme());
 
-    // check local storage for explicit theme preference, else check system.
-    if (localStorage.getItem("theme") === "dark") {
-        activateDarkTheme();
-    } else if (localStorage.getItem("theme") === "light") {
-        activateLightTheme();
-    } else {
-        activateColorScheme();
-    }
+  // check local storage for explicit theme preference, else check system.
+  if (localStorage.getItem('theme') === 'dark') {
+    activateDarkTheme();
+  } else if (localStorage.getItem('theme') === 'light') {
+    activateLightTheme();
+  } else {
+    activateColorScheme();
+  }
 }
 
 function activateTheme(theme) {
-    localStorage.setItem("theme", theme);
-    document.documentElement.setAttribute("theme", theme);
+  localStorage.setItem('theme', theme);
+  document.documentElement.setAttribute('theme', theme);
 }
 
 function activateDarkTheme() {
-    activateTheme("dark");
-    themeSwitch.checked = false;
-    mobileSun.classList.remove("active");
-    mobileSun.classList.add("inactive");
-    
-    mobileMoon.classList.remove("inactive");
-    mobileMoon.classList.add("active");
+  activateTheme('dark');
+  themeSwitch.checked = false;
 }
 
 function activateLightTheme() {
-    activateTheme("light");
-    themeSwitch.checked = true;
-    mobileMoon.classList.remove("active");
-    mobileMoon.classList.add("inactive");
-    
-    mobileSun.classList.remove("inactive");
-    mobileSun.classList.add("active");
+  activateTheme('light');
+  themeSwitch.checked = true;
 }
 
 function activateColorScheme() {
-    const isDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    const isLightTheme = window.matchMedia("(prefers-color-scheme: light)").matches;
-    const isNotSpecified = window.matchMedia("(prefers-color-scheme: no-preference)").matches;
-    const isNotSupported = !isDarkTheme && !isLightTheme && !isNotSpecified;
+  const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const isLightTheme = window.matchMedia(
+    '(prefers-color-scheme: light)'
+  ).matches;
+  const isNotSpecified = window.matchMedia(
+    '(prefers-color-scheme: no-preference)'
+  ).matches;
+  const isNotSupported = !isDarkTheme && !isLightTheme && !isNotSpecified;
 
-    if (isDarkTheme) activateDarkTheme();
-    if (isLightTheme) activateLightTheme();
-    if (isNotSpecified) {
-        console.log("Your browser doesn't have a color scheme preference. Defaulting to dark theme.");
-        activateDarkTheme();
-    }
-    if (isNotSupported) {
-        console.log("Your browser doesn't support the color scheme preference media query. Please update to a more recent browser. Defaulting to dark theme.");
-        console.log("Or, hey, maybe you're using Tor or another privacy-aware browser. Keep doing that, then.");
-        activateDarkTheme();
-    }
+  if (isDarkTheme) activateDarkTheme();
+  if (isLightTheme) activateLightTheme();
+  if (isNotSpecified) {
+    console.log(
+      "Your browser doesn't have a color scheme preference. Defaulting to dark theme."
+    );
+    activateDarkTheme();
+  }
+  if (isNotSupported) {
+    console.log(
+      "Your browser doesn't support the color scheme preference media query. Please update to a more recent browser. Defaulting to dark theme."
+    );
+    console.log(
+      "Or, hey, maybe you're using Tor or another privacy-aware browser. Keep doing that, then."
+    );
+    activateDarkTheme();
+  }
 }
 
 function toggleTheme() {
-    if (document.documentElement.getAttribute("theme") === "dark") {
-        activateLightTheme();
-    } else if (document.documentElement.getAttribute("theme") === "light") {
-        activateDarkTheme();
-    } else {
-        console.log("Looks like something broke. Setting the theme to your browser's color scheme preference.");
-        activateColorScheme();
-    }
+  if (document.documentElement.getAttribute('theme') === 'dark') {
+    activateLightTheme();
+  } else if (document.documentElement.getAttribute('theme') === 'light') {
+    activateDarkTheme();
+  } else {
+    console.log(
+      "Looks like something broke. Setting the theme to your browser's color scheme preference."
+    );
+    activateColorScheme();
+  }
 }
 
 onLoad();
